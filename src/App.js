0c86ffb419e060c6e08aa41971569dc1c2a1fee1
aidanasm <smanova707@gmail.com>
2022-07-13 19:51:46 +0600

    logic add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const handlePlus =()=>{
+    setCounter(counter+1)
+  }
+  const handleMinus =()=>{
+    setCounter(counter-1)
+  }
   return (
     <div className="App">
       <h1>Git practise lesson</h1>
-      <div className="counter">0</div>
-      <Button>+</Button>
-      <Button>-</Button>
+      <div className="counter">{counter}</div>
+      <Button handleClick={handlePlus}>+</Button>
+      <Button handleClick={handleMinus}>-</Button>
     </div>
   );
 }
